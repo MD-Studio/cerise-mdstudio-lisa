3f1a86c5b3d18226b6f47f5ba8645ed839d5af1a
felipeZ <tifonzafel@gmail.com>
2018-01-30 11:31:05 +0100

    added path to GROMACS

--- api/install.sh
+++ api/install.sh
@@ -8,3 +8,17 @@ SCRIPT="https://raw.githubusercontent.com/MD-Studio/cerise-mdstudio-share-data/m
 wget $SCRIPT -P "$CERISE_API_FILES/mdstudio/bin"
 chmod u+x "$CERISE_API_FILES/mdstudio/bin/install_share.sh"
 $CERISE_API_FILES/mdstudio/bin/install_share.sh $CERISE_API_FILES
+
+
+GMXRC_MDSTUDIO="/sara/sw/gromacs-2016.2-sp/bin/GMXRC.bash"
+
+# ADD ENV variable if it is not already there
+pred=$(grep -m 1 'GMXRC_MDSTUDIO' ~/.bashrc)
+if [[ -z $pred ]]; then
+    echo >>~/.bashrc
+    echo '# Added by cerise-mdstudio, sorry!' >>~/.bashrc
+    echo 'module load intel-studio'
+    echo 'module load gromacs/2016.2'
+    echo 'export GMXRC_MDSTUDIO=$GMXRC_MDSTUDIO' >>~/.bashrc
+    echo '# End cerise-mdstudio additions' >>~/.bashrc    
+fi
