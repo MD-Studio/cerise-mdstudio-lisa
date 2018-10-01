8108ecae8aa713b00474cc9b1f9b43f5e74207c2
felipe <tifonzafel@gmail.com>
2018-10-01 13:48:53 +0200

    modified installation and use batch queue

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+## Added
+
+ * gmxrc environmental variable
+
+## Change
+
+ * Use batch queue
+ 
+## fixed
+ 
+ * installation script
+ 
+## Remove
+
+ * unused script files
--- api/install.sh
+++ api/install.sh
@@ -1,24 +1,16 @@
+
 #!/bin/bash
 
-CERISE_API_FILES="$1"
-CERISE_DATA=cerise-mdstudio-share-data
+CERISE_API_FILES="${1}"
 
 # Install share data and miniconda
-SCRIPT="https://raw.githubusercontent.com/MD-Studio/cerise-mdstudio-share-data/master/scripts/install_share.sh"
-wget $SCRIPT -P "$CERISE_API_FILES/mdstudio/bin"
-chmod u+x "$CERISE_API_FILES/mdstudio/bin/install_share.sh"
-$CERISE_API_FILES/mdstudio/bin/install_share.sh $CERISE_API_FILES
+INSTALL_SCRIPT="${CERISE_API_FILES}/mdstudio/bin/install_share.sh"
 
-
-GMXRC_MDSTUDIO="/sara/sw/gromacs-2016.2-sp/bin/GMXRC.bash"
-
-# ADD ENV variable if it is not already there
-pred=$(grep -m 1 'GMXRC_MDSTUDIO' ~/.bashrc)
-if [[ -z $pred ]]; then
-    echo >>~/.bashrc
-    echo '# Added by cerise-mdstudio, sorry!' >>~/.bashrc
-    echo 'module load intel-studio'
-    echo 'module load gromacs/2016.2'
-    echo 'export GMXRC_MDSTUDIO=$GMXRC_MDSTUDIO' >>~/.bashrc
-    echo '# End cerise-mdstudio additions' >>~/.bashrc    
+if [ ! -f ${INSTALL_SCRIPT} ] ; then
+    # Download installation
+    SCRIPT="https://raw.githubusercontent.com/MD-Studio/cerise-mdstudio-share-data/master/scripts/install_share.sh"
+    wget ${SCRIPT} -P "${CERISE_API_FILES}/mdstudio/bin"
+    chmod u+x "${CERISE_API_FILES}/mdstudio/bin/install_share.sh"
 fi
+${INSTALL_SCRIPT} ${CERISE_API_FILES}
+
